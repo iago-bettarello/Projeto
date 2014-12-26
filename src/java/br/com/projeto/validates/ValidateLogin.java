64abbaec4dd99c3528f189eac9fa052f037224ff
iago-bettarello <iago_bettarello_@hotmail.com>
2014-12-26 18:19:52 -0200

    Controle de sessão do usuário

--- src/java/br/com/projeto/validates/ValidateLogin.java
+++ src/java/br/com/projeto/validates/ValidateLogin.java
@@ -10,6 +10,8 @@ import br.com.projeto.config.Config;
 import br.com.projeto.dbConnection.ConnectionPostgreSql;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 
 /**
